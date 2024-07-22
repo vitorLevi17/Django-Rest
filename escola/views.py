f31da41d1e134bbca42f49744d4186ef439083d4
vitorLevi17 <vitorlevimenezessantos@outlook.com>
2024-07-22 14:13:16 -0300

    Ajustando o projeto

--- escola/views.py
+++ escola/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+def estudantes(request):
+    if request.method == 'GET':
+        estudante =  {
+            'id' : '1',
+            'nome':'vitor'
+        }
+        return JsonResponse(estudante)
--- setup/settings.py
+++ setup/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'escola',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
