ffb5f8e9010a58102a8de68ddf3e56a381e3bfd7
vitorLevi17 <vitorlevimenezessantos@outlook.com>
2024-07-22 15:26:13 -0300

    Criação do ADMIN

--- escola/admin.py
+++ escola/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Estudantes, Curso
 
-# Register your models here.
+class EstudanteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'Nome', 'Email', 'CPF', 'Data_Nascimento', 'Celular')
+    list_display_links = ('id', 'Nome')
+    list_per_page = 20
+    search_fields = ('Nome',)
+
+admin.site.register(Estudantes, EstudanteAdmin)
+
+class CursoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'id_curso', 'Descricao')
+    list_display_links = ('id', 'id_curso')
+    search_fields = ('id_curso',)
+
+admin.site.register(Curso, CursoAdmin)
