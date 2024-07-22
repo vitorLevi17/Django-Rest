46f6a2201f425bd13902f9542b314cbcc11580dd
vitorLevi17 <vitorlevimenezessantos@outlook.com>
2024-07-22 14:40:05 -0300

    Criação dos models

--- escola/models.py
+++ escola/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Estudantes(models.Model):
+
+    Nome = models.CharField(max_length=100)
+    Email = models.EmailField(blank=False, max_length=50)
+    CPF = models.CharField(blank=False, max_length=11)
+    Data_Nascimento = models.DateTimeField(blank=False)
+    Celular = models.CharField(blank=False,max_length=14)
+
+    def __str__(self):
+        return self.Nome
+
+class Curso(models.Model):
+
+    NIVEL = (
+        ('B','BASICO'),
+        ('I', 'INTERMEDIARIO'),
+        ('A', 'AVANCADO'),
+    )
+
+    id_curso = models.CharField(max_length=10)
+    Descricao = models.CharField(blank=False,max_length=100)
+    Nivel = models.CharField(max_length=1,choices=NIVEL,blank=False,null=False,default='B')
+
+    def __str__(self):
+        return self.id_curso
