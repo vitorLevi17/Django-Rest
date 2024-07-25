d4df1d093e3ca6eec68df5def3450cbbfb8ddeb1
vitorLevi17 <vitorlevimenezessantos@outlook.com>
2024-07-25 18:20:04 -0300

    Limpar codigo na parte de autenticação

--- escola/views.py
+++ escola/views.py
@@ -1,36 +1,29 @@
 from .serializers import *
 from rest_framework import viewsets, generics
-from rest_framework.authentication import BasicAuthentication
-from rest_framework.permissions import IsAuthenticated
 
 class EstudantesViewSet(viewsets.ModelViewSet):
-    authentication_classes = [BasicAuthentication]
-    permission_classes = [IsAuthenticated]
+
     queryset = Estudantes.objects.all()
     serializer_class = EstudantesSerializer
 
 class CursoViewSet(viewsets.ModelViewSet):
-    authentication_classes = [BasicAuthentication]
-    permission_classes = [IsAuthenticated]
+
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
 
 class MatriculaViewSet(viewsets.ModelViewSet):
-    authentication_classes = [BasicAuthentication]
-    permission_classes = [IsAuthenticated]
+
     queryset = Matricula.objects.all()
     serializer_class = MatriculaSerializer
 class ListaMatriculaEstudante(generics.ListAPIView):
-    authentication_classes = [BasicAuthentication]
-    permission_classes = [IsAuthenticated]
+
     def get_queryset(self):
         queryset = Matricula.objects.filter(id_estudante_id=self.kwargs['pk'])
         return queryset
     serializer_class = ListaMatriculasEstudantesSerializer
 
 class ListaMatriculaCurso(generics.ListAPIView):
-    authentication_classes = [BasicAuthentication]
-    permission_classes = [IsAuthenticated]
+
     def get_queryset(self):
         queryset = Matricula.objects.filter(id_curs_id=self.kwargs['pk'])
         return queryset
