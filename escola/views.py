c39823e0a42db8037293105d1442f9ea487375ee
vitorLevi17 <vitorlevimenezessantos@outlook.com>
2024-08-29 09:25:22 -0300

    Passar objetos ordenados pelo ID

--- escola/views.py
+++ escola/views.py
@@ -4,7 +4,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 
 class EstudantesViewSet(viewsets.ModelViewSet):
 
-    queryset = Estudantes.objects.all()
+    queryset = Estudantes.objects.all().order_by("id")
     serializer_class = EstudantesSerializer
     filter_backends = [DjangoFilterBackend, filters.OrderingFilter,filters.SearchFilter]
     ordering_fields = ['Nome']
@@ -17,23 +17,22 @@ class EstudantesViewSet(viewsets.ModelViewSet):
 
 class CursoViewSet(viewsets.ModelViewSet):
 
-    queryset = Curso.objects.all()
+    queryset = Curso.objects.all().order_by("id")
     serializer_class = CursoSerializer
 
 class MatriculaViewSet(viewsets.ModelViewSet):
 
-    queryset = Matricula.objects.all()
+    queryset = Matricula.objects.all().order_by("id")
     serializer_class = MatriculaSerializer
 class ListaMatriculaEstudante(generics.ListAPIView):
 
     def get_queryset(self):
-        queryset = Matricula.objects.filter(id_estudante_id=self.kwargs['pk'])
+        queryset = Matricula.objects.filter(id_estudante_id=self.kwargs['pk']).order_by("id")
         return queryset
     serializer_class = ListaMatriculasEstudantesSerializer
-
 class ListaMatriculaCurso(generics.ListAPIView):
 
     def get_queryset(self):
-        queryset = Matricula.objects.filter(id_curs_id=self.kwargs['pk'])
+        queryset = Matricula.objects.filter(id_curs_id=self.kwargs['pk']).order_by("id")
         return queryset
     serializer_class = ListaMatriculasCursoSerializer
