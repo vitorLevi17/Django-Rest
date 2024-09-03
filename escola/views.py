79cc8cff6ce74ada472d505c090c6d358784bbe4
vitorLevi17 <vitorlevimenezessantos@outlook.com>
2024-09-03 17:43:34 -0300

    Documentação da API

--- escola/views.py
+++ escola/views.py
@@ -5,6 +5,24 @@ from rest_framework.throttling import UserRateThrottle
 from .throttle import MatriculaAnonRateThrottle
 
 class EstudantesViewSet(viewsets.ModelViewSet):
+    """
+    Descrição da ViewSet:
+    - Endpoint para CRUD de estudantes.
+
+    Campos de ordenação:
+    - nome: permite ordenar os resultados por nome.
+
+    Campos de pesquisa:
+    - nome: permite pesquisar os resultados por nome.
+    - cpf: permite pesquisar os resultados por CPF.
+
+    Métodos HTTP Permitidos:
+    - GET, POST, PUT, PATCH, DELETE
+
+    Classe de Serializer:
+    - EstudanteSerializer: usado para serialização e desserialização de dados.
+    - Se a versão da API for 'v2', usa EstudanteSerializerV2.
+    """
 
     queryset = Estudantes.objects.all().order_by("id")
     serializer_class = EstudantesSerializer
@@ -18,12 +36,29 @@ class EstudantesViewSet(viewsets.ModelViewSet):
         return EstudantesSerializer
 
 class CursoViewSet(viewsets.ModelViewSet):
+    """
+    Descrição da ViewSet:
+    - Endpoint para CRUD de cursos.
+
+    Métodos HTTP Permitidos:
+    - GET, POST, PUT, PATCH, DELETE
+    """
 
     queryset = Curso.objects.all().order_by("id")
     serializer_class = CursoSerializer
 
-
 class MatriculaViewSet(viewsets.ModelViewSet):
+    """
+    Descrição da ViewSet:
+    - Endpoint para CRUD de matrículas.
+
+    Métodos HTTP Permitidos:
+    - GET, POST
+
+    Throttle Classes:
+    - MatriculaAnonRateThrottle: limite de taxa para usuários anônimos.
+    - UserRateThrottle: limite de taxa para usuários autenticados.
+    """
 
     queryset = Matricula.objects.all().order_by("id")
     serializer_class = MatriculaSerializer
@@ -32,12 +67,24 @@ class MatriculaViewSet(viewsets.ModelViewSet):
     http_method_names = ["get", "post"]
 
 class ListaMatriculaEstudante(generics.ListAPIView):
-
+    """
+        Descrição da View:
+        - Lista Matriculas por id de Estudante
+        Parâmetros:
+        - pk (int): O identificador primário do objeto. Deve ser um número inteiro.
+        """
     def get_queryset(self):
         queryset = Matricula.objects.filter(id_estudante_id=self.kwargs['pk']).order_by("id")
         return queryset
     serializer_class = ListaMatriculasEstudantesSerializer
+
 class ListaMatriculaCurso(generics.ListAPIView):
+    """
+       Descrição da View:
+       - Lista Matriculas por id de Curso
+       Parâmetros:
+       - pk (int): O identificador primário do objeto. Deve ser um número inteiro.
+       """
 
     def get_queryset(self):
         queryset = Matricula.objects.filter(id_curs_id=self.kwargs['pk']).order_by("id")
--- setup/urls.py
+++ setup/urls.py
@@ -2,6 +2,23 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from escola.views import *
+from drf_yasg import openapi
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Documentação API",
+      default_version='v1',
+      description="Documentação da API escola",
+      terms_of_service="https://www.google.com/policies/terms/", # os 3 abaixo podem ser alterados
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+)
+
 
 router = DefaultRouter(trailing_slash=False)
 router.register(prefix='estudantes',viewset=EstudantesViewSet,basename='Estudantes')
@@ -15,4 +32,7 @@ urlpatterns = [
     path('',include(router.urls)),
     path('estudantes/<int:pk>/matriculas/',ListaMatriculaEstudante.as_view()),
     path('cursos/<int:pk>/matriculas/',ListaMatriculaCurso.as_view()),
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc')
 ]
